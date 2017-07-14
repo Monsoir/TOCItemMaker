a78943dd9741b0b081a18171418b04a75fe50e71
Yeung Mon <monwingyeung@gmail.com>
2017-07-14 11:32:16 +0800

    What: 1. 初始化 2. 添加代码文件

---
+++ maker.py
@@ -0,0 +1,43 @@
+import pyperclip
+import re
+
+# 这些字符需要使用空字符串代替
+CharacterToBeDiscarded = ['.', '/', '(', ')', '，', '>', '<']
+
+# 这些字符需要使用 - 代替
+CharacterToBeCross = [' ']
+
+# 去除首尾空格
+def trimSpace(rawMaterial):
+    temp = rawMaterial.strip()
+    return temp
+
+# 大写转小写
+def lower(rawMaterial):
+    return rawMaterial.lower()
+
+# 将部分字符使用 `-` 代替
+def toCross(rawMaterial):
+    targets = ''.join(CharacterToBeCross)
+    return re.sub('[%s]' % targets, '-', rawMaterial)
+
+# 将部分字符去掉
+def discardCharacter(rawMaterial):
+    targets = ''.join(CharacterToBeDiscarded)
+    return re.sub('[%s]' % targets, '', rawMaterial)
+
+def transferToItem(rawMaterial):
+    if len(rawMaterial) > 0:
+        temp = trimSpace(rawMaterial)
+        temp = lower(temp)
+        temp = toCross(temp)
+        temp = discardCharacter(temp)
+        return temp
+
+    return None
+
+if __name__ == '__main__':
+    rawMaterial = pyperclip.paste()
+    target = transferToItem(rawMaterial)
+    if target:
+        pyperclip.copy(target)
